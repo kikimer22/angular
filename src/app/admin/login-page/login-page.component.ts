044b9587463c43aea5771bea64b763ef2f10098e
rsalnikov <ruslan.salnikov@p2h.com>
2020-09-26 12:10:11 +0300

    in progress

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from '../../shared/interfaces';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -7,8 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  ngOnInit(): void {
+  constructor(
+    public auth: AuthService,
+    private router: Router
+  ) {
   }
 
+  public form: FormGroup;
+  public submitted: boolean;
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    });
+  }
+
+  submit(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.submitted = true;
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
+    };
+
+    this.auth.login(user).subscribe(() => {
+      this.form.reset();
+      this.router.navigate(['/']);
+      this.submitted = false;
+    }, () => {
+      this.submitted = false;
+    });
+  }
 }
 
